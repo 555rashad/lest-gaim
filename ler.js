487bdf746f4bb4d76908ccea6a22fabde5073e48
555rashad <91946415+555rashad@users.noreply.github.com>
2021-10-23 19:51:37 +0300

    Add files via upload

---
+++ ler.js
@@ -0,0 +1,28 @@
+
+let player=["football","tennis",""];
+let computer=["computer","iphone","tablet"];
+let result=["winner","Loser"];
+
+
+for(p=0;p=player.length;p++){
+
+}
+
+ function player(player){
+     
+     document.getElementById('play').innerHTML="l hodhv hgufm";
+ }
+
+  function computer(computer){
+     document.getElementById('playr').innerHTML="l hodhv hgufm";
+ }
+ function result(result){
+     if(player==="football"&& computer==="computer"){
+        document.getElementById('plays').innerHTML="winner ";
+     }else{
+        document.getElementById('plays').innerHTML="loser";
+     }
+     
+function Reboot(){
+    document.getElementById('playe').innerHTML="l hodhv hgufm";
+ }
